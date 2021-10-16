1df9b42f5897ab2dd8eac3d9730e95e304994e1c
Deepak Gupta <88889634+dgupta966@users.noreply.github.com>
2021-10-16 22:41:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Reducer from "./component/reducer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>useReducer Hook</h1>
+      <Reducer />
+
+      <div style={{ margin: "20px", color: "blue" }}>@deepakgupta</div>
+    </div>
+  );
+}
